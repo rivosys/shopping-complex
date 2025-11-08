5d2447da1e5a1f9e2ab626ad1556101aad835fde
rivosys <rivosys@gmail.com>
2025-11-08 16:22:03 +0530

    refactor: restructure project for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
     ignoreDuringBuilds: true
   },
   images: {
-    domains: ['images.unsplash.com'],
     unoptimized: true,
     remotePatterns: [
       {
@@ -17,7 +16,7 @@ const nextConfig = {
     ]
   },
   distDir: '.next',
-  output: 'export',
+  output: 'standalone',
   experimental: {
     appDir: true,
     optimizePackageImports: ['@mui/icons-material', '@mui/material']
